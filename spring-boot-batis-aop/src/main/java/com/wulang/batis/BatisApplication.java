f838d250a3dfda0c72293af31be7141e841cbc87
wulang <sanliangitch96@gmail.com>
2019-12-31 12:38:41 +0800

    ✨ spring-boot-batis-aop完成完成

--- spring-boot-batis-aop/src/main/java/com/wulang/batis/BatisApplication.java
+++ spring-boot-batis-aop/src/main/java/com/wulang/batis/BatisApplication.java
@@ -3,6 +3,9 @@ package com.wulang.batis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *
+ */
 @SpringBootApplication
 public class BatisApplication {
 
