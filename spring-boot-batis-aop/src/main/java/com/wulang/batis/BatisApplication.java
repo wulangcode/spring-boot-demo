4f307f53715c93cdde784fb7fec32e1e4cd1a06d
wulang <sanliangitch96@gmail.com>
2019-12-31 11:10:13 +0800

    启动类

---
+++ spring-boot-batis-aop/src/main/java/com/wulang/batis/BatisApplication.java
@@ -0,0 +1,12 @@
+package com.wulang.batis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatisApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(BatisApplication.class, args);
+    }
+}
