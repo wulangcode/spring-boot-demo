f51df73549eeb7eb342e5f4d7551793e29ae9f78
wulang <sanliangitch96@gmail.com>
2019-12-31 11:12:39 +0800

    MybatisPlusConfig配置文件

---
+++ spring-boot-batis-aop/src/main/java/com/wulang/batis/common/config/MybatisPlusConfig.java
@@ -0,0 +1,34 @@
+package com.wulang.batis.common.config;
+
+import com.baomidou.mybatisplus.core.injector.ISqlInjector;
+import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author wulang
+ * @create 2019/12/13/10:52
+ */
+@EnableTransactionManagement
+@Configuration
+@MapperScan("com.wulang.batis.*.dao")
+public class MybatisPlusConfig {
+    /**
+     * 注入sql注入器
+     */
+    @Bean
+    public ISqlInjector sqlInjector() {
+        return new LogicSqlInjector();
+    }
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+}
